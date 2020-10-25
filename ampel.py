ce67599babc258899abee0c5cdd287a3cbcc1ccc
schnusch <schnusch@users.noreply.github.com>
2020-10-25 13:07:11 +0100

    compute SSIM and only commit files with SSIM < 0.99339

--- ampel.py
+++ ampel.py
@@ -5,7 +5,10 @@ import shlex
 import subprocess
 import time
 
+import cv2
+import numpy
 from selenium import webdriver
+from skimage.metrics import structural_similarity
 
 def init_log(logfile):
 	logging.basicConfig(format='[%(asctime)s] %(name)s %(levelname)s %(message)s', level=logging.INFO)
@@ -17,7 +20,7 @@ def init_log(logfile):
 		finally:
 			os.close(fd)
 
-def take_screenshots(day, week):
+def take_screenshots():
 	options = webdriver.firefox.options.Options()
 	options.headless = True
 	driver = webdriver.Firefox(options=options)
@@ -29,18 +32,41 @@ def take_screenshots(day, week):
 		ampel = ampel.find_element_by_xpath('../../..//iframe')
 
 		time.sleep(15)
-		logging.info('taking screenshot %r...', day)
-		ampel.screenshot(day)
+		logging.info("taking screenshot of today's incidence...")
+		day = ampel.screenshot_as_png
 
 		driver.switch_to.frame(ampel)
 		inzidenz = driver.find_element_by_xpath('//*[text()="7-Tage-Inzidenz"]')
 		inzidenz.click()
 
 		driver.switch_to.default_content()
-		logging.info('taking screenshot %r...', week)
-		ampel.screenshot(week)
+		logging.info("taking screenshot week's incidence...")
+		week = ampel.screenshot_as_png
 	finally:
 		driver.close()
+	return (day, week)
+
+def compare_images(old_name, new_data):
+	try:
+		old_img = cv2.imread(old_name)
+	except FileNotFoundError:
+		return -1
+	new_img = numpy.frombuffer(new_data, dtype=numpy.uint8)
+	new_img = cv2.imdecode(new_img, cv2.IMREAD_UNCHANGED)
+
+	old_img = cv2.cvtColor(old_img, cv2.COLOR_BGR2GRAY)
+	new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+	score, _ = structural_similarity(old_img, new_img, full=True)
+	return score
+
+def write_changed_images(*imgs):
+	for name, new_data in imgs:
+		score = compare_images(name, new_data)
+		logging.info("%s's SSIM: %s", name, score)
+		if score < 0.99339:
+			logging.info('%s changed', name)
+			with open(name, 'wb') as fp:
+				fp.write(new_data)
 
 def run(*cmd):
 	logging.info('running %s...', ' '.join(map(shlex.quote, cmd)))
@@ -48,9 +74,10 @@ def run(*cmd):
 	return p.returncode == 0
 
 if __name__ == '__main__':
-	imgs = ('inzidenz.png', '7-tage.png')
 	init_log(None)
-	take_screenshots(*imgs)
+	imgs = ('inzidenz.png', '7-tage.png')
+	screenshots = take_screenshots()
+	write_changed_images(*zip(imgs, screenshots))
 	now = time.strftime('%Y-%m-%d %H:%M %Z')
 	run('git', 'add', '--', *imgs) \
 		and run('git', 'commit', '-m', 'updated screenshots ' + now) \
--- default.nix
+++ default.nix
@@ -2,6 +2,9 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "coronaampel-dresden";
   buildInputs = with pkgs; [
+    git
+    python3Packages.opencv4
     python3Packages.selenium
+    python3Packages.scikitimage
   ];
 }
