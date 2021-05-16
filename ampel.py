98e7d684c65adaee7a88b08219e871bc19718004
schnusch <schnusch@users.noreply.github.com>
2021-05-16 18:34:23 +0200

    save HTML instead of screenshot

--- ampel.py
+++ ampel.py
@@ -9,6 +9,7 @@ import cv2
 import numpy
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import WebDriverWait
 from skimage.metrics import structural_similarity
 
 def init_log(logfile):
@@ -21,27 +22,61 @@ def init_log(logfile):
 		finally:
 			os.close(fd)
 
-def take_screenshots():
+def resize_window(driver):
+	W, H = 960, 720
+
+	widget = driver.find_element_by_css_selector('.widget')
+
+	w = int(widget.get_attribute('offsetWidth'),  10)
+	h = int(widget.get_attribute('offsetHeight'), 10)
+	dw = W - w
+	dh = H - h
+	x = driver.get_window_size()
+	driver.set_window_size(x['width'] + dw, x['height'] + dh)
+
+	while True:
+		w = int(widget.get_attribute('offsetWidth'),  10)
+		h = int(widget.get_attribute('offsetHeight'), 10)
+		if (w, h) == (W, H):
+			break
+		dw = 1 if w < W else -1
+		dh = 1 if h < H else -1
+		x = driver.get_window_size()
+		driver.set_window_size(x['width'] + dw, x['height'] + dh)
+
+def extract_widgets():
 	options = webdriver.firefox.options.Options()
 	options.headless = True
 	driver = webdriver.Firefox(options=options)
 	try:
 		logging.info('loading Corona-Ampel...')
 		driver.get('https://stva-dd.maps.arcgis.com/apps/opsdashboard/index.html#/903c42db63184099a4da5e23c4e732b3')
-		time.sleep(15)
+		# when the SVG contains text it should be loaded
+		# wait a second to be sure
+		WebDriverWait(driver, 30).until(lambda d: (
+			d.find_element_by_tag_name('svg').find_element_by_tag_name('text'),
+			time.sleep(1),
+		))
+		resize_window(driver)
 
 		# find active tab
-		tab = driver.find_element_by_xpath('//*[contains(@class, "is-active")]')
+		tab = driver.find_element_by_css_selector('.is-active')
 
 		for _ in range(4):
 			# get current caption
 			caption = tab.get_attribute('textContent')
 			caption = ' '.join(caption.split())
-			filename = caption + '.png'
+			filename = caption + '.html'
 
-			# take screenshot
-			logging.info('taking screenshot of %r...', caption)
-			yield (filename, driver.get_screenshot_as_png())
+			driver.execute_script('''
+				(({style, offsetWidth, offsetHeight}) => {
+					style.width  = offsetWidth  + "px";
+					style.height = offsetHeight + "px";
+				})(document.querySelector(".widget"));
+			''')
+			html = driver.find_element_by_css_selector('.widget')
+			html = html.get_attribute('outerHTML')
+			yield (filename, html)
 
 			# next tab
 			try:
@@ -49,36 +84,47 @@ def take_screenshots():
 			except NoSuchElementException:
 				break
 			tab.click()
+			time.sleep(0.5)
 	finally:
 		driver.close()
 
-def compare_images(old_name, new_data):
-	if not os.path.isfile(old_name):
-		return -1
-	old_img = cv2.imread(old_name)
-	new_img = numpy.frombuffer(new_data, dtype=numpy.uint8)
-	new_img = cv2.imdecode(new_img, cv2.IMREAD_UNCHANGED)
-
-	old_img = cv2.cvtColor(old_img, cv2.COLOR_BGR2GRAY)
-	new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
-
-	if new_img.shape != old_img.shape:
-		return -1
-
-	score, _ = structural_similarity(old_img, new_img, full=True)
-	return score
-
-def write_changed_images(imgs):
-	for name, new_data in imgs:
-		if new_data is None:
-			logging.warning("skipping %s...", name)
-			continue
-		score = compare_images(name, new_data)
-		logging.info("%s's SSIM: %s", name, score)
-		if score < 0.99339:
-			logging.info('%s changed', name)
-			with open(name, 'wb') as fp:
-				fp.write(new_data)
+def write_html(htmls):
+	index_html = '''<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<style>
+			body {
+				min-width: 960px;
+				margin: 0 auto;
+				background: #222;
+				color: #fff;
+				font-family: sans-serif;
+			}
+		</style>
+	</head>
+	<body>
+'''
+	for name, html in htmls:
+		index_html += '<h1>' + name[:-5].replace('&', '&amp;').replace('<', '&lt;') + '</h1>'
+		index_html += html
+		with open(name, 'w', encoding='utf-8') as fp:
+			fp.write('''<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+	</head>
+	<body style="background: #222; margin: 0; font-family: sans-serif;">
+''' + html + '''
+	</body>
+</html>
+''')
+	index_html += '''
+	</body>
+</html>
+'''
+	with open('index.html', 'w', encoding='utf-8') as fp:
+		fp.write(index_html)
 
 def run(*cmd):
 	logging.info('running %s...', ' '.join(map(shlex.quote, cmd)))
@@ -87,8 +133,9 @@ def run(*cmd):
 
 if __name__ == '__main__':
 	init_log(None)
-	screenshots = dict(take_screenshots())
-	write_changed_images(screenshots.items())
+	widgets = dict(extract_widgets())
+	write_html(widgets.items())
 	now = time.strftime('%Y-%m-%d %H:%M %Z')
-	run('git', 'commit', '-m', 'updated screenshots ' + now, '--', *screenshots.keys()) \
+	run('git', 'add', '--', 'index.html', *widgets.keys()) \
+		and run('git', 'commit', '-m', 'updated screenshots ' + now) \
 		and run('git', 'push')
