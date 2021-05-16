5ef7494539ad799b560ee693885d389d5885165c
schnusch <schnusch@users.noreply.github.com>
2021-05-16 10:53:52 +0200

    use RKI-only dashboard

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Corona-Ampel
 
-regelmäßige Screenshots der [Dresdner Corona-Ampel](https://www.dresden.de/de/leben/gesundheit/hygiene/infektionsschutz/corona.php)
+Screenshots der [Dresdner Corona-Ampel](https://www.dresden.de/de/leben/gesundheit/hygiene/infektionsschutz/corona.php)
 
-| RKI | Dresden |
-| --- | ------- |
-| ![Heute RKI](https://github.com/club-aquarium/corona-dresden/raw/ampel/Heute%20RKI.png) | ![Heute Dresden](https://github.com/club-aquarium/corona-dresden/raw/ampel/Heute%20Dresden.png) |
-| ![Wochenverlauf RKI](https://github.com/club-aquarium/corona-dresden/raw/ampel/Wochenverlauf%20RKI.png) | ![Wochenverlauf Dresden](https://github.com/club-aquarium/corona-dresden/raw/ampel/Wochenverlauf%20Dresden.png) |
+| Dresden | Sachsen |
+| ------- | ------- |
+| ![Heute Dresden (RKI)](https://github.com/club-aquarium/corona-dresden/raw/ampel/Heute%20Dresden%20(RKI).png) | ![Heute Sachsen (RKI)](https://github.com/club-aquarium/corona-dresden/raw/ampel/Heute%20Sachsen%20(RKI).png) |
+| ![Wochenverlauf Dresden (RKI)](https://github.com/club-aquarium/corona-dresden/raw/ampel/Wochenverlauf%20Dresden%20(RKI).png) | ![Wochenverlauf Sachsen (RKI)](https://github.com/club-aquarium/corona-dresden/raw/ampel/Wochenverlauf%20Sachsen%20(RKI).png) |
--- ampel.py
+++ ampel.py
@@ -27,42 +27,28 @@ def take_screenshots():
 	driver = webdriver.Firefox(options=options)
 	try:
 		logging.info('loading Corona-Ampel...')
-		driver.get('https://experience.arcgis.com/experience/d2386f3214c1451c81b242be69bb3d50')
+		driver.get('https://stva-dd.maps.arcgis.com/apps/opsdashboard/index.html#/903c42db63184099a4da5e23c4e732b3')
 		time.sleep(15)
 
-		# find ampel tab-bar
-		driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
-		try:
-			tabbar = driver.find_element_by_xpath('''
-				//*[text()="Heute RKI"]
-				/ancestor::
-				*[contains(@class, "collapsed-bar-container")]
-			''')
-		finally:
-			driver.switch_to.default_content()
+		# find active tab
+		tab = driver.find_element_by_xpath('//*[contains(@class, "is-active")]')
 
 		for _ in range(4):
 			# get current caption
-			driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
-			try:
-				caption = tabbar.find_element_by_xpath('./*[2]')
-				caption = caption.get_attribute('textContent')
-				caption = ' '.join(caption.split())
-				filename = caption + '.png'
-			finally:
-				driver.switch_to.default_content()
+			caption = tab.get_attribute('textContent')
+			caption = ' '.join(caption.split())
+			filename = caption + '.png'
 
 			# take screenshot
 			logging.info('taking screenshot of %r...', caption)
 			yield (filename, driver.get_screenshot_as_png())
 
-			# click arrow right
-			driver.switch_to.frame(driver.find_element_by_tag_name('iframe'))
+			# next tab
 			try:
-				right_btn = tabbar.find_element_by_xpath('./*[last()]')
-				right_btn.click()
-			finally:
-				driver.switch_to.default_content()
+				tab = tab.find_element_by_xpath('./following-sibling::*')
+			except NoSuchElementException:
+				break
+			tab.click()
 	finally:
 		driver.close()
 
@@ -73,11 +59,12 @@ def compare_images(old_name, new_data):
 	new_img = numpy.frombuffer(new_data, dtype=numpy.uint8)
 	new_img = cv2.imdecode(new_img, cv2.IMREAD_UNCHANGED)
 
+	old_img = cv2.cvtColor(old_img, cv2.COLOR_BGR2GRAY)
+	new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
+
 	if new_img.shape != old_img.shape:
 		return -1
 
-	old_img = cv2.cvtColor(old_img, cv2.COLOR_BGR2GRAY)
-	new_img = cv2.cvtColor(new_img, cv2.COLOR_BGR2GRAY)
 	score, _ = structural_similarity(old_img, new_img, full=True)
 	return score
 
@@ -103,6 +90,5 @@ if __name__ == '__main__':
 	screenshots = dict(take_screenshots())
 	write_changed_images(screenshots.items())
 	now = time.strftime('%Y-%m-%d %H:%M %Z')
-	run('git', 'add', '--', *screenshots.keys()) \
-		and run('git', 'commit', '-m', 'updated screenshots ' + now) \
+	run('git', 'commit', '-m', 'updated screenshots ' + now, '--', *screenshots.keys()) \
 		and run('git', 'push')
